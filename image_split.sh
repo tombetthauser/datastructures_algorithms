3cc1c20f8cf97eff3c8a2860bbcefb5a49773722
tombetthauser <tombetthauser@gmail.com>
2020-06-02 22:27:10 -0700

    Copy image working

--- image_split.sh
+++ image_split.sh
@@ -31,10 +31,23 @@ get_width()
   echo ${ARR2[0]}
 }
 
+quad_image()
+{
+  ffmpeg -i $1 "copy_$1"
+}
+
 clear
 
 printf "\n\nEnter image file in current folder to process (with file extension): "
 read file_name
+
+quad_image $file_name
+
+# printf "\n\nEnter height of sign surface in inches with decimals (ie 15.25): "
+# read sign_height
+
+# printf "\n\nEnter width of sign surface in inches with decimals (ie 12.5): "
+# read sign_width
 
 echo $(get_height $file_name)
 echo $(get_width $file_name)
