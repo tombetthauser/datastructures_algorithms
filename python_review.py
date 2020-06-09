cb8a91a355bb5616d12b3b45f6eb76a77ca1128f
tombetthauser <tombetthauser@gmail.com>
2020-06-08 22:43:57 -0700

    Add notes on dir and help

--- python_review.py
+++ python_review.py
@@ -40,5 +40,56 @@ Print Me!
 """
 
 # z = """No Print Me!""" # LEGAL but prints without space above and below
+# print(z) # LEGAL, prints z with space above and below!
 
-print(z) # Prints z with space above and below!
+# These are STRING LITERALS which are just strings
+# Charactors are not treated dynamically until the quotes end
+# They are taken Literally
+
+
+
+
+
+### VARIABLES
+
+# Python has no command for declaring variables
+
+_2pac = 1999 # Totally legal but terrible
+__foo__ = 'FOO' # Legal and less terrible but dont do it stupid
+
+# print(_2pac)
+# print(__foo__)
+
+# x = 5
+# print(type(x))
+
+
+
+# METHOD vs FUNCTION
+
+# import time
+# print(time.time())
+
+# print() is a function
+# .time() is a method
+
+
+
+# DIR() and HELP()
+
+'''
+  DIR() looks up the directory of methods for a particular datatype
+
+  dir(str)
+  dir(int)
+  dir(float)
+  etc...
+
+  dir(__builtins__) # shows all built in functions
+
+
+  HELP() looks up the functionality of an individual method
+
+  help(str.strip)
+  help(list.append)
+...
